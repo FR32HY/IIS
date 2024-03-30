e239ef1a810572fb4434df6ae4cd539d2e5abc2c
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-03-30 13:29:38 +1000

    LECTURE - Изучение обобщенных классов

---
+++ v.glagolev/6_Generic_classes_and_methods/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Generic_classes_and_methods
+{
+	//internal class Person
+	//{
+	//	public int Id { get; set; }
+	//	public string Name { get; set; }
+
+	//	public Person(int id, string name)
+	//	{
+	//		Id = id;
+	//		Name = name;
+	//	}
+	//}
+	internal class Person
+	{
+		public object Id { get; set; }
+		public string Name { get; set; }
+		public Person(object id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+	}
+	class MyPerson
+	{
+		void Main()
+		{
+			Person tom = new Person(546, "Tom");
+			Person bob = new Person("abc123", "Bob");
+			int tomId = (int)tom.Id;
+			string bobId = (string)bob.Id;
+            Console.WriteLine(tomId);
+			Console.WriteLine(bobId);
+		}
+	}
+}
---
+++ v.glagolev/6_Generic_classes_and_methods/PersonNew.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Generic_classes_and_methods
+{
+	internal class PersonNew<T>
+	{
+		public static T? code;
+		public T Id { get; set; }
+		public string Name { get; set; }
+
+        public PersonNew(T id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+	class Company<P>
+	{
+		public P CEO { get; set; }
+        public Company(P ceo)
+        {
+			CEO = ceo;
+        }
+    }
+	class MyPersonNew
+	{
+		void Main()
+		{
+			PersonNew<int> tom = new PersonNew<int>(546, "Tom");
+			PersonNew<int>.code = 123;
+
+			PersonNew<string> bob = new PersonNew<string>("abc123", "Bob");
+			PersonNew<string>.code = "meta";
+
+			int tomId = tom.Id;
+			string bobId = bob.Id;
+			Console.WriteLine(tomId);
+			Console.WriteLine(bobId);
+
+			//------------------------
+			Company<PersonNew<int>> microsoft = new Company<PersonNew<int>>(tom);
+            Console.WriteLine(microsoft.CEO.Id);
+			Console.WriteLine(microsoft.CEO.Name);
+		}
+	}
+
+}
