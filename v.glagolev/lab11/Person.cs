65e1ae5eeb562ed97923bd0f91c9750105e033fc
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-05-03 22:19:14 +1000

    Lab - Код лабораторной работы 11

---
+++ v.glagolev/lab11/Person.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using System.Xml.Linq;
+
+namespace lr11
+{
+	public enum GenderType
+	{
+		Male,
+		Female
+	}
+	public class Person
+	{
+		public string ID { get; set; }
+		public string Fname { get; set; }
+		public string Lname { get; set; }
+		public string Email { get; set; }
+		public GenderType Gender { get; set; }
+		public float Salary { get; set; }
+		public bool HasChildren { get; set; }
+		public static Person Create(string str)
+		{
+			Person p = new Person();
+			string[] e = str.Split(',');
+			p.ID = e[0].Trim();
+			p.Fname = e[1].Trim();
+			p.Lname = e[2].Trim();
+			p.Email = e[3].Trim();
+			string tmp = e[4].Trim();
+			if (tmp == "Male")
+				p.Gender = GenderType.Male;
+			else p.Gender = GenderType.Female;
+			p.Salary = Convert.ToSingle(e[5].TrimStart('$').Replace('.', ','));
+			p.HasChildren = Convert.ToBoolean(e[6].TrimStart());
+			return p;
+
+		}
+		public override string ToString()
+		{
+			String s = string.Format(
+			"ID: {0}, Имя: {1}, Фамилия: {2}, ({3})\n" +
+			"E-mail: {4}, 3/n: {5}, Дети: {6}",
+					ID, Fname, Lname, GenderToStr(Gender),
+						Email, Salary, HasChildrenToStr(HasChildren));
+			return s;
+		}
+		private static String GenderToStr(GenderType g)
+		{
+			if (g == GenderType.Male) return "M";
+			else return "Ж";
+		}
+		private static String HasChildrenToStr(Boolean g)
+		{
+			if (g == true) return "Есть";
+			else return "Нет";
+		}
+
+	}
+}
---
+++ v.glagolev/lab11/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace lr11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StreamReader f_in = new StreamReader("data_example_small.csv");
+
+#if !DEBUG
+		TextWriter save_out = Console.Out;
+		var new_out = new StreamWriter(@"lr11_output.txt");
+		Console.SetOut(new_out);
+#endif
+			List<Person> all = new List<Person>();
+
+			//string line = f_in.ReadLine();
+			//while ((line = f_in.ReadLine()) != null)
+			//{
+			//	Console.WriteLine(line);
+			//}
+
+			try
+			{
+				string line = f_in.ReadLine();
+				while ((line = f_in.ReadLine()) != null)
+				{
+					all.Add(Person.Create(line));
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			Console.WriteLine("Всего пользователей: {0}", all.Count);
+			foreach (var p in all)
+				Console.WriteLine(p);
+			//1.Найдем количество пользователей мужского и женского пола
+			Console.WriteLine("****** Задача 1 **********");
+			int mCount = all.FindAll(p => p.Gender == GenderType.Male).ToList().Count;
+			Console.WriteLine("KonH4ecTBO мужчин: {0}", mCount);
+			Console.WriteLine("KonM4ecTBO женщин: {0}", all.Count - mCount);
+
+			//2.Требуется найти самую высокую зарплату среди женщин и среди мужчин (вывести этих пользователей)
+			Console.WriteLine("****** Задача 2 **********");
+			float male_max = (from p in all
+							  where p.Gender == GenderType.Male
+							  select p.Salary).Max();
+			Person rich_man = (from p in all
+							   where (p.Gender == GenderType.Male) &&
+							   (p.Salary == male_max)
+							   select p).First();
+			Console.WriteLine(rich_man);
+			float female_max = (from p in all
+								where p.Gender == GenderType.Female
+								select p.Salary).Max();
+			Person rich_woman = (from p in all
+								 where (p.Gender == GenderType.Female) &&
+								 (p.Salary == female_max)
+								 select p).First();
+			Console.WriteLine(rich_woman);
+			//3.Требуется посчитать количество пользователей с детьми и без
+			Console.WriteLine("\n****** Задача 3 *******");
+			int Count_Yes = 0;
+			int Count_No = 0;
+			for (int i = 0; i < all.Count; i++)
+			{
+				if (all[i].HasChildren) Count_Yes++;
+				else Count_No++;
+			}
+			Console.WriteLine("C детьми: {0}, без детей {1}", Count_Yes, Count_No);
+			//4.посчитать суммарный фонд заработной платы для четырех категорий, образованных признаками Gender и HasChildren
+			Console.WriteLine("\n****** Задача 4 **********");
+			float salary_total = (from p in all
+								  select p.Salary).Sum();
+			float m_haschildren = (from p in all
+								   where (p.Gender == GenderType.Male) && p.HasChildren
+								   select p.Salary).Sum();
+			float f_haschildren = (from p in all
+								   where (p.Gender == GenderType.Female) && p.HasChildren
+								   select p.Salary).Sum();
+			float m_nochildren = (from p in all
+								  where (p.Gender == GenderType.Male) && !p.HasChildren
+								  select p.Salary).Sum();
+			float f_nochildren = (from p in all
+								  where (p.Gender == GenderType.Female) && !p.HasChildren
+								  select p.Salary).Sum();
+			var f = System.Globalization.CultureInfo.GetCultureInfo("en-us");
+			Console.WriteLine("Фонд з/п: {0}\n" +
+			"Муж.+дети: {1}.,\t Жен.+дети: {2}\n" +
+			"Муж.-дети: {3}.\t Жен.-дети: {4}",
+			salary_total.ToString("C3", f),
+			m_haschildren.ToString("C", f), f_haschildren.ToString("C", f),
+			m_nochildren.ToString("C", f), f_nochildren.ToString("C", f));
+
+#if !DEBUG
+		Console.SetOut(save_out);
+		new_out.Close();
+#else
+			Console.ReadKey();
+#endif
+		}
+	}
+
+}
