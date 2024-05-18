42372e5132d05ef49e22b3eb622b22c8f71a2f15
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-05-18 13:12:52 +1000

    LAB16 -  Добавлены файлы

---
+++ v.glagolev/lab16/RawDataItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab16
+{
+	public class RawDataItem
+	{
+		public String Name { get; set; }
+		public int Group { get; set; }
+		public String Part { get; set; }
+		public float Price { get; set; }
+		public float Count { get; set; }
+		public float Sum
+		{
+			get
+			{
+				return Count * Price;
+			}
+		}
+	}
+}
